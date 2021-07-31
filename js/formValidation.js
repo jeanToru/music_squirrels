4393c9e61542f0d6fd70204b7a1e25c8aadcd3f0
Jean Brown <jeantoruno00@gmail.com>
2021-07-31 10:45:19 -0700

    Prueba database

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,3 +1,35 @@
-import { getUsers } from "./apiUsers.js";
+// import { getUsers } from "./apiUsers.js";
 
-getUsers()
+const validation = document.getElementById('createUser');
+
+function createUser(name, email, password) {
+    const newUser = {
+        name: name,
+        email: email,
+        password: password,
+    };
+    create(newUser);
+}
+
+function create(user) {
+    console.log(JSON.stringify(user));
+    fetch('https://squirrelsmusic.herokuapp.com/user', {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify(user),
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+        })
+    alert(`El usuario fue agregado satisfactoriamente`);
+}
+
+validation.addEventListener('submit', (event) => {
+    event.preventDefault();
+    createUser(validation.elements[0].value, validation.elements[1].value, validation.elements[2].value)
+})
