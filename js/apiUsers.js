890aa6cd90c0f6f44017bee96208f2d40c940dc3
Jean Brown <jeantoruno00@gmail.com>
2021-07-31 14:04:20 -0700

    Database and Frond-end

--- js/apiUsers.js
+++ js/apiUsers.js
@@ -1,24 +1,53 @@
 const urlUsers = 'https://squirrelsmusic.herokuapp.com/users'
+const ulrUser = 'https://squirrelsmusic.herokuapp.com/user'
 
-function getUsers() {
+function create(user) {
+
+  fetch(ulrUser, {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+}
+
+function signUp(email, password) {
+  fetch(`${urlUsers}/${email}`, {
+
+    })
+}
+
+function checkData(email, password, name) {
   fetch(urlUsers, {
-    method: "GET"
+    method: "GET",
   })
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok " + response.status);
-    }
-
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data)
-  })
-  .catch((error) => {
-    console.log("error", error);
-  });
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      for (let i = 0; i < data.data.length; i++) {
+        if (email === data.data[i].email) {
+          console.log('Email utilizado por otro usuario')
+          break
+        }
+        const newUser = {
+          "name": `${name}`,
+          "email": `${email}`,
+          "password": `${password}`,
+        }
+        create(newUser);
+      }
+    })
 }
 
 export {
-  getUsers
+  checkData,
+  signUp
 }
--- js/formValidation.js
+++ js/formValidation.js
@@ -1,35 +1,26 @@
-// import { getUsers } from "./apiUsers.js";
+import { checkData, signUp } from "./apiUsers.js";
 
 const validation = document.getElementById('createUser');
+const login = document.getElementById('login');
 
-function createUser(name, email, password) {
-    const newUser = {
-        name: name,
-        email: email,
-        password: password,
-    };
-    create(newUser);
-}
 
-function create(user) {
-    console.log(JSON.stringify(user));
-    fetch('https://squirrelsmusic.herokuapp.com/user', {
-        headers: {
-            "Content-Type": "application/json"
-        },
-        method: "POST",
-        body: JSON.stringify(user),
-    })
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-        })
-    alert(`El usuario fue agregado satisfactoriamente`);
-}
+validation.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const password = validation.elements[2].value;
+  const passwordConfirm = validation.elements[3].value;
+  const userName = validation.elements[0].value;
+  const userEmail = validation.elements[1].value;
+  if (password === passwordConfirm){
+    checkData(userEmail, password, userName)
+  }else {
+    alert('Playo la contra')
+  }
+});
 
-validation.addEventListener('submit', (event) => {
-    event.preventDefault();
-    createUser(validation.elements[0].value, validation.elements[1].value, validation.elements[2].value)
-})
+login.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const passwordConfirm = {
+    password: login.elements[0].value
+  }
+  signUp(login.elements[0].value, passwordConfirm);
+});
